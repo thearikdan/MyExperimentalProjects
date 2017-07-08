77675d347a38ad2d263ff4c73d1025f46947a1ca
Ara Danielyan <arikdan@gmail.com>
2017-07-07 22:39:05 -0400

    Added perceptron_2 class and tests

---
+++ Perceptron/perceptron_2.py
@@ -0,0 +1,34 @@
+import numpy as np
+
+class Perceptron_2:
+    def __init__(self, w, b):
+        self.w = w
+        self.b = b
+
+    @property
+    def w(self):
+        return self.w
+
+    @property
+    def b(self):
+        return self.b
+
+    def inp(self, x):
+        return self.w * x + self.b
+
+    def __activation(self, t):
+        return np.tanh(t)
+
+    def out(self, x):
+        return self.__activation(self.inp(x))
+
+    def activation_deriv(self, x):
+        t = np.tanh(x)**2
+        return 1.0 - t
+
+    def b_deriv(self, x):
+        return self.activation_deriv(self.inp(x))
+  
+    def w_deriv(self, x):
+        return self.activation_deriv(self.inp(x)) * x
+
