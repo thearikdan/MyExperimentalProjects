84e25d6b2116a7d3703269724c91f926a1ae637c
Ara Danielyan <arikdan@gmail.com>
2017-08-28 12:09:14 -0400

    Added point cloud generation

---
+++ python-plyfile/square.py
@@ -0,0 +1,26 @@
+import numpy as np
+from plyfile import PlyElement, PlyData
+
+point_count = 2048
+x_low = 0.
+x_high = 100.
+
+y_low = 0.
+y_high = 0.
+
+z_low = 0.
+z_high = 100.
+
+vertex = np.zeros((2048,),
+                      dtype=[('x', 'f4'), ('y', 'f4'),
+                             ('z', 'f4')])
+
+for i in range (point_count):
+    vertex[i] = (np.random.uniform(x_low, x_high), np.random.uniform(y_low, y_high), np.random.uniform(z_low, z_high))
+
+print vertex
+
+
+el = PlyElement.describe(vertex, 'vertex')
+
+PlyData([el]).write('square.ply')
--- python-plyfile/test.py
+++ python-plyfile/test.py
@@ -8,13 +8,7 @@ vertex = numpy.array([(0, 0, 0),
                       dtype=[('x', 'f4'), ('y', 'f4'),
                              ('z', 'f4')])
 
-face = numpy.array([([0, 1, 2], 255, 255, 255),
-                     ([0, 2, 3], 255,   0,   0),
-                     ([0, 1, 3],   0, 255,   0),
-                     ([1, 2, 3],   0,   0, 255)],
-                    dtype=[('vertex_indices', 'i4', (3,)),
-                           ('red', 'u1'), ('green', 'u1'),
-                           ('blue', 'u1')])
+print vertex.shape
 
 el = PlyElement.describe(vertex, 'vertex')
 
