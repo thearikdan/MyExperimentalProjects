bfae3ce166caf56588b41c6ad870d2bb91cd7485
daniela <ara.danielyan@autodesk.com>
2017-09-08 13:40:26 -0400

    Working on input pipeline

--- ParamToPointCloud/ParamToPointCloudGenerator.py
+++ ParamToPointCloud/ParamToPointCloudGenerator.py
@@ -5,7 +5,7 @@ from os.path import isfile, join
 
 DATA_DIR = '/raid/MyProjects/python-point-cloud/data/boxes'
 BATCH_SIZE = 2
-MAX_PARAM_COUNT = 8 #maximum numbe of parameters for a primitive, including label
+#MAX_PARAM_COUNT = 8 #maximum numbe of parameters for a primitive, including label
 PARAMS_VECTOR_SIZE = 256 #sparse vector for holding param values for different primitives
 
 labels = {"box":0,
@@ -20,17 +20,21 @@ data_files = [f for f in listdir(DATA_DIR) if isfile(join(DATA_DIR, f))]
 
 def get_primitive_params_pos_start_from_label(label):
     label_count = len(labels)
-    index = labels[label]
-    pos = PARAMS_VECTOR_SIZE / label_count * int(index)
+    pos = PARAMS_VECTOR_SIZE / label_count * label
     return pos
 
 
-def get_primitive_file_label_params(data_dir, name):
+def get_primitive_file_label_params(name):
     params = name.split('_')
     count = len(params)
-    p = np.zeros(MAX_PARAM_COUNT)
+    p = np.zeros(PARAMS_VECTOR_SIZE)
+    label = int (labels[params[0]])
+    pos = get_primitive_params_pos_start_from_label(label)
+    p[pos] = label
+    for i in range (pos + 1, pos + count - 1): #count -1 because last param is the ".ply" extension
+        p[i] = int (params[i])
 
-    print params
+    return p
 
 
 
@@ -38,16 +42,9 @@ def get_point_clouds_labels_params(data_dir, files):
     arr = np.array(files)
     np.random.shuffle(arr)
     count = arr.size
-    labels = np.zeros(count)
-    p1 = np.zeros(count)
-    p2 = np.zeros(count)
-    p3 = np.zeros(count)
-    p4 = np.zeros(count)
-    p5 = np.zeros(count)
     for i in range(count):
         file_name = arr[i]
-        get_primitive_file_label_params(data_dir, file_name)
-        params = file_name.split('_')
+        params = get_primitive_file_label_params(file_name)
         print params
     
     return arr
@@ -61,12 +58,12 @@ for i in xrange(0, count, BATCH_SIZE):
     print "Batch " + str(i / BATCH_SIZE)
     print arr[i:i+BATCH_SIZE]
 
-label_count = len(labels)
-print label_count
+#label_count = len(labels)
+#print label_count
 
-for i in range(label_count):
-    label = labels.keys()[i]
-    pos = get_primitive_params_pos_start_from_label(label)
-    print label
-    print pos
+#for i in range(label_count):
+#    label = labels.keys()[i]
+#    pos = get_primitive_params_pos_start_from_label(label)
+#    print label
+#    print pos
 
