e4de796cec57390ab6fb9428ae26c61ea5ff32e9
Ara Danielyan <arikdan@gmail.com>
2017-11-22 20:59:09 -0500

    Finished reading utilities

--- pyfin/input/read.py
+++ pyfin/input/read.py
@@ -1,3 +1,4 @@
+import numpy as np
 from numpy import genfromtxt
 
 def get_all_data_from_file(filename):
@@ -10,12 +11,59 @@ def get_headers_from_file(filename):
     return data[0]
 
 
-def get_timeframe_from_file(filename):
-    data = get_all_data_from_file(filename)
-    return data[:,0:1]
-
-
-def get_numerical_data_from_file(filename):
+def get_numeric_data_from_file(filename):
     data = get_all_data_from_file(filename)
     return data[1:,1:]
+
+
+def get_date_from_file(filename):
+    data = get_all_data_from_file(filename)
+    return data[1:,0:1]
+
+def get_opening_price_from_numeric_data(data):
+    return data[:,0:1].astype(np.float)
+
+def get_high_price_from_numeric_data(data):
+    return data[:,1:2].astype(np.float)
+
+def get_low_price_from_numeric_data(data):
+    return data[:,2:3].astype(np.float)
+
+def get_closing_price_from_numeric_data(data):
+    return data[:,3:4].astype(np.float)
+
+
+def get_adjusted_closing_price_from_numeric_data(data):
+    return data[:,4:5].astype(np.float)
+
+def get_volume_from_numeric_data(data):
+    return data[:,5:6].astype(np.float)
+
+def get_opening_price_from_file(filename):
+    data = get_numeric_data_from_file(filename)
+    return get_opening_price_from_numeric_data(data)
+
+
+def get_high_price_from_file(filename):
+    data = get_numeric_data_from_file(filename)
+    return get_high_price_from_numeric_data(data)
+
+
+def get_low_price_from_file(filename):
+    data = get_numeric_data_from_file(filename)
+    return get_low_price_from_numeric_data(data)
+
+def get_closing_price_from_file(filename):
+    data = get_numeric_data_from_file(filename)
+    return get_closing_price_from_numeric_data(data)
+
+def get_adjusted_closing_price_from_file(filename):
+    data = get_numeric_data_from_file(filename)
+    return get_adjusted_closing_price_from_numeric_data(data)
+
+
+def get_volume_from_file(filename):
+    data = get_numeric_data_from_file(filename)
+    return get_volume_from_numeric_data(data)
+
 
