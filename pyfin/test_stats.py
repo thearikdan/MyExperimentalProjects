dc02eb591efb102778fd173acaa6228cb19c6309
Ara Danielyan <arikdan@gmail.com>
2017-11-26 22:03:51 -0500

    Added padding

--- pyfin/test_stats.py
+++ pyfin/test_stats.py
@@ -3,21 +3,21 @@ from stats import percentage
 from viz import colormap
 import numpy as np
 
-name = '/raid/data/pyfin/WEED.TO.csv'
+
+
+#name = '/raid/data/pyfin/WEED.TO.csv'
+#name = '/media/ara/HDD/data/Finance/WEED.TO.10.22.17.11.22.17.csv'
+name = '/media/ara/HDD/data/Finance/WEED.TO_month.csv'
 
 data = read.get_all_data_from_file(name)
 
-date = read.get_date_from_all_data(my_data)
+date = read.get_date_from_all_data(data)
 nd = read.get_numeric_data_from_all_data(data)
 
 pc = percentage.get_percentage_change_from_numeric_data(nd)
 perc = pc * 100
 
-#sh = np.shape(perc)
-#print sh
+shaped_perc = colormap.reshape_data(perc, 5)
 
-perc_shaped = perc.reshape(-1, 5)
-print perc_shaped	
-
-colormap.show(date, perc_shaped)
+colormap.show(shaped_perc)
 
--- pyfin/viz/colormap.py
+++ pyfin/viz/colormap.py
@@ -4,42 +4,77 @@ import matplotlib.pyplot as plt
 from matplotlib import colors
 import numpy as np
 
+LARGE_NEGATIVE_NUMBER = -1000.
+
+
+def reshape_data(data, mod):
+    lst = data.tolist()
+    l = len(lst)
+
+    rest = l % mod
+    print rest
+    for i in range(mod - rest):
+        lst.append([LARGE_NEGATIVE_NUMBER])
+
+    new_data = np.array(lst)
+
+    data_shaped = new_data.reshape(-1, 5)
+    print data_shaped	
+    sh = np.shape(data_shaped)
+    print sh
+    return data_shaped
+    
 
 def show(data):
     shape = np.shape(data)
 
     # create discrete colormap
-#    bounds = [-10, -9, -8, -7, -6, -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+#   bounds = [-10, -9, -8, -7, -6, -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+#   bounds = [-20, -10, -9, -8, -7, -6, -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 20]
+
     bounds = []
+    bounds.append(LARGE_NEGATIVE_NUMBER)
 
-    for i in xrange (-100, -30, 5):
+    for i in xrange (-1000, -300, 100):
         bounds.append(i / 10.)
 
-    for i in xrange (-30, 30, 1):
+    for i in xrange (-300, -100, 10):
         bounds.append(i / 10.)
 
-    for i in xrange (30, 100, 5):
+    for i in xrange (-100, -50, 5):
         bounds.append(i / 10.)
- 
-#    bounds = [-10, -9, -8, -7, -6, -5, -4, -3.5, -3, -2.5 -2, -1.5, -1, -0.5, 0, 0.5, 1, 1.5, 2, 2.5, #3, 3.5, 4, 5, 6, 7, 8, 9, 10]
-#    print bounds
-    count = len(bounds)
+
+    for i in xrange (-50, 50, 1):
+        bounds.append(i / 10.)
+
+    for i in xrange (50, 100, 5):
+        bounds.append(i / 10.)
+
+    for i in xrange (100, 300, 10):
+        bounds.append(i / 10.)
+
+    for i in xrange (300, 1000, 100):
+        bounds.append(i / 10.)
+
+    count = len(bounds) - 1
 
     colormap = []
+    c = [0., 0., 1.]
+    colormap.append(c)
+
     step = 1. / (count / 2)
 
-    for i in range (count / 2):
+    for i in range (1, count / 2):
         c = [1.0, i * step, i * step]
         colormap.append(c)
 
-    c = [1., 1., 1.]
-    colormap.append(c)
-
     for i in range (count / 2 + 1, count):
         c = [2 - (i + 1) * step, 1, 2 - (i + 1) * step]
+#        c = [2 - (i) * step, 1, 2 - (i) * step]
         colormap.append(c)
 
-#    print colormap
+
+    print colormap
 
     cmap = colors.ListedColormap(colormap)
     norm = colors.BoundaryNorm(bounds, cmap.N)
@@ -49,8 +84,9 @@ def show(data):
 
     # draw gridlines
     ax.grid(which='major', axis='both', linestyle='-', color='k', linewidth=2)
-    ax.set_xticks(np.arange(-0.5, shape[0]-0.5, 1));
-    ax.set_yticks(np.arange(-0.5, shape[1]-0.5, 1));
+#    ax.set_xticks(np.arange(-0.5, shape[0] + 0.5, 1));
+    ax.set_xticks(np.arange(-0.5, 5, 1));
+    ax.set_yticks(np.arange(-0.5, shape[1] - 0.5, 1));
 
     plt.show()
 
