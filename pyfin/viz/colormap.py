aa7a7cb7b713c8f9c82c79d33f8cc5a1070272fd
Ara Danielyan <arikdan@gmail.com>
2017-12-03 21:14:29 -0500

    Added colors to heatmap

--- pyfin/viz/colormap.py
+++ pyfin/viz/colormap.py
@@ -3,68 +3,13 @@
 import matplotlib.pyplot as plt
 from matplotlib import colors
 import numpy as np
-from utils import constants
+import color_schema
 
 
 def show(data, mod):
     shape = np.shape(data)
 
-    # create discrete colormap
-#   bounds = [-10, -9, -8, -7, -6, -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-#   bounds = [-20, -10, -9, -8, -7, -6, -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 20]
-
-    bounds = []
-    bounds.append(constants.PADDED_DAY)
-    bounds.append(constants.HOLIDAY - constants.BORDER)
-
-
-    for i in xrange (-1000, -300, 100):
-        bounds.append(i / 10.)
-
-    for i in xrange (-300, -100, 10):
-        bounds.append(i / 10.)
-
-    for i in xrange (-100, -50, 5):
-        bounds.append(i / 10.)
-
-    for i in xrange (-50, -20, 2):
-        bounds.append(i / 10.)
-
-    for i in xrange (-20, 20, 1):
-        bounds.append(i / 10.)
-
-    for i in xrange (20, 50, 2):
-        bounds.append(i / 10.)
-
-    for i in xrange (50, 100, 5):
-        bounds.append(i / 10.)
-
-    for i in xrange (100, 300, 10):
-        bounds.append(i / 10.)
-
-    for i in xrange (300, 1000, 100):
-        bounds.append(i / 10.)
-
-    count = len(bounds) - 2
-
-#    print bounds
-
-    colormap = []
-    c = constants.PADDED_DAY_COLOR
-    colormap.append(c)
-    c = constants.HOLIDAY_COLOR
-    colormap.append(c)
-
-    step = 1. / (count / 2)
-
-    for i in range (0, count / 2):
-        c = [1.0, i * step, i * step]
-        colormap.append(c)
-
-    for i in range (count / 2 + 1, count):
-        c = [2 - (i + 1) * step, 1, 2 - (i + 1) * step]
-        colormap.append(c)
-
+    bounds, colormap = color_schema.get_bounds_and_colormap()
 
 #    print colormap
 
--- pyfin/viz/heatmap.py
+++ pyfin/viz/heatmap.py
@@ -1,15 +1,25 @@
 #Needs pyfin virtualenv for matplotlib
-
 import matplotlib.pyplot as plt
 import seaborn as sns
+import pandas as pd
 import numpy as np
+import color_schema
+from matplotlib import colors
 
 def show(data, mod):
     shape = np.shape(data)
-    sns.set()
+
+    bounds, colormap = color_schema.get_bounds_and_colormap()
+
+    cmap = colors.ListedColormap(colormap)
+    norm = colors.BoundaryNorm(bounds, cmap.N)
+
+    # Create a dataset (fake)
+    df = pd.DataFrame(data, columns=["Mon","Tue","Wed","Thu","Fri"])
+
 
     # Draw a heatmap with the numeric values in each cell
-    f, ax = plt.subplots(figsize=(9, 5))
-    sns.heatmap(data, annot=True, fmt="f", linewidths=.5, ax=ax)
+    f, ax = plt.subplots(figsize=(shape[0], shape[1]))
+    sns.heatmap(df, annot=True, fmt="f", linewidths=.5, ax=ax, cmap=cmap, norm=norm)
     plt.show()
 
