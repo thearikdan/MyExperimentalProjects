a436ec9851c19eb0ea3b780525c3bd2d16ce590a
Ara Danielyan <arikdan@gmail.com>
2017-12-03 20:39:32 -0500

    Started working on seaborn heatmap

--- pyfin/stats_viz_heatmap.py
+++ pyfin/stats_viz_heatmap.py
@@ -1,16 +1,16 @@
 from input import read
 from stats import percentage 
-from viz import colormap
-from utils import time
+from viz import heatmap
+from utils import time, shape
 import numpy as np
 
 
 
-name = '/raid/data/pyfin/LEAF.TO.csv'
+#name = '/raid/data/pyfin/LEAF.TO.csv'
 #name = '/raid/data/pyfin/AMZN.csv'
 
 #name = '/media/ara/HDD/data/Finance/ACB.TO.csv'
-#name = '/media/ara/HDD/data/Finance/WEED.TO_1_month.csv'
+name = '/media/ara/HDD/data/Finance/WEED.TO_1_month.csv'
 #name = '/media/ara/HDD/data/Finance/AMZN_month.csv'
 
 
@@ -28,8 +28,8 @@ nd = read.get_numeric_data_from_all_data(data)
 pc = percentage.get_percentage_change_from_numeric_data(nd)
 perc = pc * 100
 
-shaped_perc = colormap.reshape_data(perc, days, 5)
+shaped_perc = shape.reshape_data(perc, days, 5)
 #print shaped_perc
 
-colormap.show(shaped_perc, 5)
+heatmap.show(shaped_perc, 5)
 
--- pyfin/viz/heatmap.py
+++ pyfin/viz/heatmap.py
@@ -1,27 +1,15 @@
 #Needs pyfin virtualenv for matplotlib
+
 import matplotlib.pyplot as plt
+import seaborn as sns
 import numpy as np
 
-def show(data):
-    rows = list('1234') #rows categories
-    columns = list('MF') #column categories
-    fig,ax=plt.subplots()
+def show(data, mod):
+    shape = np.shape(data)
+    sns.set()
 
-    #Advance color controls
-    ax.pcolor(data,cmap=plt.cm.Reds,edgecolors='k')
-    ax.set_xticks(np.arange(0,2)+0.5)
-    ax.set_yticks(np.arange(0,4)+0.5)
-
-    # Here we position the tick labels for x and y axis
-    ax.xaxis.tick_bottom()
-    ax.yaxis.tick_left()
-
-    #Values against each labels
-    ax.set_xticklabels(columns,minor=False,fontsize=20)
-    ax.set_yticklabels(rows,minor=False,fontsize=20)
-
+    # Draw a heatmap with the numeric values in each cell
+    f, ax = plt.subplots(figsize=(9, 5))
+    sns.heatmap(data, annot=True, fmt="f", linewidths=.5, ax=ax)
     plt.show()
 
-#Generate a random number, you can refer your data values also
-data = np.random.rand(4,2)
-show(data)
