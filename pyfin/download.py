e3cd3188cf3ffd29e00989dff1ae0288c5242889
Ara Danielyan <arikdan@gmail.com>
2017-12-14 10:35:02 -0500

    Working on download

--- pyfin/download.py
+++ pyfin/download.py
@@ -1,6 +1,20 @@
 import pandas_datareader as pdr
 from datetime import datetime
+import dateutil
 
-ibm = pdr.get_data_yahoo(symbols='IBM', start=datetime(2000, 1, 1), end=datetime(2012, 1, 1))
+ticker = "IBM"
+today = datetime.today()
+print today 
+
+yesterday = today + dateutil.relativedelta.relativedelta(days=-1)
+print yesterday
+
+month_ago = today + dateutil.relativedelta.relativedelta(months=-1)
+print month_ago
+
+#ibm = pdr.get_data_yahoo(symbols=ticker, start=yesterday, end=month_ago)
+#print(ibm['Adj Close'])
+
+ibm = pdr.get_data_yahoo(symbols=ticker, start=datetime(2017, 12, 13), end=datetime(2017, 11, 14))
 print(ibm['Adj Close'])
 
