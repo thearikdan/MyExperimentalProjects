0d0b76299cade46f783488ad4cf05cf9a09376be
Ara Danielyan <arikdan@gmail.com>
2017-12-27 20:27:29 -0500

    Added hour granularity

--- pyfin/intraday.py
+++ pyfin/intraday.py
@@ -2,13 +2,18 @@ from input import read
 import time
 from datetime import datetime
 
-now = datetime.now()
-now_int = int(round(time.mktime(now.timetuple())))
-start_date = datetime(2017, 12, 20)
-start_date_int = int(round(time.mktime(start_date.timetuple())))
+#now = datetime.now()
+#now_int = int(round(time.mktime(now.timetuple())))
+start_date = datetime(2017, 12, 27, 9, 30)
+start_date_tuple = start_date.timetuple()
+start_date_int = int(time.mktime(start_date_tuple))
+
+end_date = datetime(2017, 12, 27, 16, 00)
+end_date_tuple = end_date.timetuple()
+end_date_int = int(time.mktime(end_date_tuple))
 
 
-timestamp, volume , open, close, high, low = read.get_intraday_data_from_web("WEED.TO", start_date_int, now_int, "1m")
+timestamp, volume , open, close, high, low = read.get_intraday_data_from_web("WEED.TO", start_date_int, end_date_int, "1m")
 print open
 
 count = len(timestamp)
