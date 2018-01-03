2d42dad4dcf0da0bd1671254e20b52eb41e855af
Ara Danielyan <arikdan@gmail.com>
2018-01-03 18:49:18 -0500

    Added daily chart

---
+++ pyfin/daily_chart.py
@@ -0,0 +1,20 @@
+from read_write import read
+from datetime import datetime
+import matplotlib.pyplot as plt
+import numpy as np
+
+#start_date = datetime(2018, 1, 3, 9, 30)
+#end_date = datetime(2018, 1, 3, 16, 00)
+
+start_date = datetime(2017, 12, 28, 9, 30)
+end_date = datetime(2017, 12, 28, 16, 00)
+
+is_data_available, date_time, volume , opn, close, high, low = read.get_intraday_data("WEED.TO", start_date, end_date, "1m")
+
+if not (is_data_available):
+    exit(0)
+
+count = len(close)
+range = np.arange(count)
+plt.plot(range, close)
+plt.show()
