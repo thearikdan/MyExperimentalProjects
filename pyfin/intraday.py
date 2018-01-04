d8880ac1cd1f944fdb454ff3c312423059b5668c
Ara Danielyan <arikdan@gmail.com>
2018-01-03 20:03:26 -0500

    Implemented intraday percentage changes

--- pyfin/intraday.py
+++ pyfin/intraday.py
@@ -1,27 +1,15 @@
 from read_write import read
 from datetime import datetime
 import matplotlib.pyplot as plt
-import numpy as np
+from stats import percentage
 
-start_date = datetime(2017, 12, 28, 9, 30)
-end_date = datetime(2017, 12, 28, 16, 00)
+start_date = datetime(2017, 12, 28, 10, 30)
+end_date = datetime(2017, 12, 28, 11, 30)
 
-#is_data_available, date_time, volume , opn, close, high, low = read.get_intraday_data_from_web("WEED.TO", start_date, end_date, "1m")
 is_data_available, date_time, volume , opn, close, high, low = read.get_intraday_data("WEED.TO", start_date, end_date, "1m")
 
 if not (is_data_available):
     exit(0)
 
-high_price = max(close)
-print high_price
-
-ind = close.index(high_price)
-print ind
-
-max_time = date_time[ind]
-print max_time
-
-count = len(close)
-range = np.arange(count)
-plt.bar(range, close)
-plt.show()
+date_time_1, volume_per , open_per, close_per, high_per, low_per = percentage.get_percentage_change_in_intraday_prices(date_time, volume , opn, close, high, low)
+print close_per
--- pyfin/stats/percentage.py
+++ pyfin/stats/percentage.py
@@ -1,4 +1,5 @@
 from read_write import read
+import numpy as np
 
 
 
@@ -45,3 +46,20 @@ def get_high_low_percentage_change_from_numeric_data(data):
     per = diff / lp
     return per
 
+
+def get_intraday_percentage_change(list):
+    count = len(list)
+    list_begin = np.array(list[0:count-1]).astype(float)
+    list_end = np.array(list[1:count]).astype(float)
+    perc = (list_end - list_begin) / list_begin
+    return perc
+
+
+def get_percentage_change_in_intraday_prices(date_time, volume , opn, close, high, low):
+    count = len(date_time)
+    volume_per = get_intraday_percentage_change(volume)
+    open_per = get_intraday_percentage_change(opn)
+    close_per = get_intraday_percentage_change(close)
+    high_per = get_intraday_percentage_change(high)
+    low_per = get_intraday_percentage_change(low)
+    return date_time[1:count], volume_per, open_per, close_per, high_per, low_per
