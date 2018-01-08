4afe902d34d1dbd6b4b78c11b24f3e8d279d97f9
Ara Danielyan <arikdan@gmail.com>
2018-01-07 19:47:49 -0500

    Added time to plot

--- pyfin/high_low_price_time.py
+++ pyfin/high_low_price_time.py
@@ -4,9 +4,11 @@ from utils import time_op
 import matplotlib.pyplot as plt
 import numpy as np
 from stats import absolute
+import matplotlib.ticker as ticker
 
-start_date = datetime(2017, 12, 28, 9, 30)
-end_date = datetime(2017, 12, 28, 15, 59)
+
+start_date = datetime(2018, 1, 4, 9, 30)
+end_date = datetime(2018, 1, 4, 15, 59)
 
 
 is_data_available, date_time, volume , opn, close, high, low = read.get_intraday_data("WEED.TO", start_date, end_date, "1m")
@@ -35,7 +37,9 @@ for i in range (min_count):
     print close_N[min_index_list[i]]
     print date_time_N[min_index_list[i]]
 
-count_N = len(close_N)
-range = np.arange(count_N)
-plt.plot(range, close_N)
+
+plt.plot(date_time_N, close_N)
+plt.gcf().autofmt_xdate()
+
+
 plt.show()
