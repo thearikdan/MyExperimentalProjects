1cfee0de1b79d071bf467a3904110aeb9d8aacdf
Ara Danielyan <arikdan@gmail.com>
2018-01-24 09:18:31 -0500

    Added tests

--- pyfin/test/test_interpolate_by_distance.py
+++ pyfin/test/test_interpolate_by_distance.py
@@ -5,9 +5,9 @@ sys.path.insert(0, "../")
 from utils import interpolation
 
 a = [[1, 2, 3, 4, 5], [6, 7, 8, 9, 10]]
-weights = [0.1, 0.2]
+distances = [0.1, 0.2]
 
-b = interpolation.interpolate_by_distance(a, weights, 1)
+b = interpolation.interpolate_by_distance(a, distances, 1)
 print b
 
 if (b == [1, 2, 3, 4, 5]):
@@ -16,7 +16,7 @@ else:
     print "Test1 failed"
 
 
-c = interpolation.interpolate_by_distance(a, weights, 2)
+c = interpolation.interpolate_by_distance(a, distances, 2)
 print c
 
 if (c == [2.6666666666666665, 3.666666666666666, 4.666666666666666, 5.666666666666666, 6.666666666666666]):
@@ -25,7 +25,7 @@ else:
     print "Test2 failed"
 
 
-d = interpolation.interpolate_by_distance(a, weights, 3)
+d = interpolation.interpolate_by_distance(a, distances, 3)
 print d
 
 if (d == [2.6666666666666665, 3.666666666666666, 4.666666666666666, 5.666666666666666, 6.666666666666666]):
