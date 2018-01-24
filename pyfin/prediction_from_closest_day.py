89d5860cd7ee8268a4ad3bb782a337c6809455bd
Ara Danielyan <arikdan@gmail.com>
2018-01-24 12:11:43 -0500

    Working on prediction

--- pyfin/prediction_from_closest_day.py
+++ pyfin/prediction_from_closest_day.py
@@ -2,13 +2,15 @@ from datetime import datetime
 from utils import prediction, interpolation
 from stats import percentage
 from read_write import read
+import matplotlib.pyplot as plt
 
 
 
-start_date = datetime(2018, 1, 22, 9, 30)
-end_date = datetime(2018, 1, 22, 10, 30)
 
-days_count = 18
+start_date = datetime(2018, 1, 24, 9, 30)
+end_date = datetime(2018, 1, 24, 11, 50)
+
+days_count = 30
 
 symbol = "WEED.TO"
 #symbol = "EMHTF"
@@ -16,6 +18,17 @@ symbol = "WEED.TO"
 #symbol = "ACBFF"
 #symbol = "MEDFF"
 #symbol = "AMZN"
+
+
+is_data_available, date_time_curr, volume_curr , opn_curr, close_curr, high_curr, low_curr = read.get_intraday_data(symbol, start_date, end_date, "1m")
+
+if not (is_data_available):
+    exit(0)
+
+print close_curr
+count_curr = len(close_curr)
+last_price = close_curr[count_curr - 1]
+print last_price
 
 start, end, dist = prediction.get_closest_distances_time_to_predict_and_distances(symbol, start_date, end_date, days_count)
 
@@ -44,12 +57,25 @@ for i in range (count):
     low_per_list.append(low_per)
 
 count = len(date_time_per_list)
-interp_count = 5
+interp_count = 10
 
 if (interp_count > count):
     interp_count = count
 
 interp_volume_per, interp_open_per, interp_close_per, interp_high_per, interp_low_per = interpolation.interpolate_prices_and_volume_by_distance(volume_per_list, open_per_list, close_per_list, high_per_list, low_per_list, dist, interp_count)
 
+#print interp_close_per
 
-print interp_close_per
+#print date_time_per_list[0]
+#print len(date_time_per_list[0])
+
+predicted_prices = prediction.project_percentage_change(last_price, interp_close_per)
+#print len(predicted_prices)
+#print predicted_prices
+
+plt.plot(date_time_per_list[0], predicted_prices)
+plt.gcf().autofmt_xdate()
+
+plt.show()
+
+
