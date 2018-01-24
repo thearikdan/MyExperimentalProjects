efa055b079625b6f69afa57cff1a14c779833121
Ara Danielyan <arikdan@gmail.com>
2018-01-23 21:52:05 -0500

    Refactoring

--- pyfin/prediction_from_closest_day.py
+++ pyfin/prediction_from_closest_day.py
@@ -1,64 +1,8 @@
 from datetime import datetime
-from utils import prediction
+from utils import prediction, interpolation
 from stats import percentage
 from read_write import read
-import numpy as np
 
-def multiply_list_by_num(list1, num):
-    count1 = len(list1)
-    for i in range(count1):
-        list[i] = (list1[i] * num)
-    return list1
-
-
-def divide_list_by_num(list1, num):
-    count1 = len(list1)
-    for i in range(count1):
-        list[i] = (list1[i] / num)
-    return list1
-
-
-def interpolate_by_distance(my_list, dist, interp_count):
-    list_count = len(my_list)
-    dist_count = len(dist)
-    if (list_count != dist_count):
-        raise Exception('Distances and lists must have the same length!')
-
-    if (interp_count > list_count):
-        interp_count = list_count
-
-    weights = []
-    for i in range (dist_count):
-        if (dist[i] == 0):
-            #exact match, no need to interpolate
-            interp = my_list[i]
-            return interp
-        else:
-            weight = 1 / dist[i]
-            weights.append(weight)
-
-    weight_sum = sum(weights)
-
-    interp = []
-    for i in range (list_count):
-        list_np = np.array(my_list[i])
-        w = float(weights[i])
-        list_np = list_np * w
-        list_np = list_np / weight_sum
-        interp.append(list_np.tolist())
-
-    return interp
-
-
-
-
-def interpolate_prices_and_volume_by_distance(volume_per_list, open_per_list, close_per_list, high_per_list, low_per_list, dist, interp_count):
-    interp_volume_per = interpolate_by_distance(volume_per_list, dist, interp_count)
-    interp_open_per = interpolate_by_distance(open_per_list, dist, interp_count)
-    interp_close_per = interpolate_by_distance(close_per_list, dist, interp_count)
-    interp_high_per = interpolate_by_distance(high_per_list, dist, interp_count)
-    interp_low_per = interpolate_by_distance(low_per_list, dist, interp_count)
-    return (interp_volume_per, interp_open_per, interp_close_per, interp_high_per, interp_low_per)
 
 
 start_date = datetime(2018, 1, 22, 9, 30)
@@ -105,7 +49,7 @@ interp_count = 5
 if (interp_count > count):
     interp_count = count
 
-interp_volume_per, interp_open_per, interp_close_per, interp_high_per, interp_low_per = interpolate_prices_and_volume_by_distance(volume_per_list, open_per_list, close_per_list, high_per_list, low_per_list, dist, interp_count)
+interp_volume_per, interp_open_per, interp_close_per, interp_high_per, interp_low_per = interpolation.interpolate_prices_and_volume_by_distance(volume_per_list, open_per_list, close_per_list, high_per_list, low_per_list, dist, interp_count)
 
 
 print interp_close_per
---
+++ pyfin/utils/interpolation.py
@@ -0,0 +1,45 @@
+import numpy as np
+
+
+def interpolate_by_distance(my_list, dist, interp_count):
+    list_count = len(my_list)
+    dist_count = len(dist)
+    if (list_count != dist_count):
+        raise Exception('Distances and lists must have the same length!')
+
+    if (interp_count > list_count):
+        interp_count = list_count
+
+    weights = []
+    for i in range (dist_count):
+        if (dist[i] == 0):
+            #exact match, no need to interpolate
+            interp = my_list[i]
+            return interp
+        else:
+            weight = 1 / dist[i]
+            weights.append(weight)
+
+    weight_sum = sum(weights)
+
+    interp = []
+    for i in range (list_count):
+        list_np = np.array(my_list[i])
+        w = float(weights[i])
+        list_np = list_np * w
+        list_np = list_np / weight_sum
+        interp.append(list_np.tolist())
+
+    return interp
+
+
+
+def interpolate_prices_and_volume_by_distance(volume_per_list, open_per_list, close_per_list, high_per_list, low_per_list, dist, interp_count):
+    interp_volume_per = interpolate_by_distance(volume_per_list, dist, interp_count)
+    interp_open_per = interpolate_by_distance(open_per_list, dist, interp_count)
+    interp_close_per = interpolate_by_distance(close_per_list, dist, interp_count)
+    interp_high_per = interpolate_by_distance(high_per_list, dist, interp_count)
+    interp_low_per = interpolate_by_distance(low_per_list, dist, interp_count)
+    return (interp_volume_per, interp_open_per, interp_close_per, interp_high_per, interp_low_per)
+
+
