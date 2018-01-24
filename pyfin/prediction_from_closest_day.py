c08d5a338248c4cc29fde8929ac8f65044ce62e6
Ara Danielyan <arikdan@gmail.com>
2018-01-24 18:50:21 -0500

    Refactored prediction

--- pyfin/prediction_from_closest_day.py
+++ pyfin/prediction_from_closest_day.py
@@ -1,58 +1,6 @@
 from datetime import datetime
-from utils import prediction, interpolation
-from stats import percentage
-from read_write import read
+from utils import prediction
 import matplotlib.pyplot as plt
-
-
-
-def get_linear_interpolation_prediction(symbol, from_date_time, to_date_time, days_count_to_analyse, top_days_count_to_interpolate):
-    
-    is_data_available, date_time_curr, volume_curr , opn_curr, close_curr, high_curr, low_curr = read.get_intraday_data(symbol, start_date, end_date, "1m")
-
-    if not (is_data_available):
-        return None
-
-    count_curr = len(close_curr)
-    last_price = close_curr[count_curr - 1]
-
-    start, end, dist = prediction.get_closest_distances_time_to_predict_and_distances(symbol, from_date_time, to_date_time, days_count_to_analyse)
-
-    count = len(start)
-
-    date_time_per_list = []
-    volume_per_list = []
-    open_per_list = []
-    close_per_list = []
-    high_per_list = []
-    low_per_list = []
-
-    for i in range (count):
-        is_data_available, date_time, volume , opn, close, high, low = read.get_intraday_data(symbol, start[i], end[i], "1m")
-        
-        if not (is_data_available):
-            continue
-
-        date_time_per, volume_per, open_per, close_per, high_per, low_per = percentage.get_percentage_change_in_intraday_prices(date_time, volume , opn, close, high, low)
-
-        date_time_per_list.append(date_time_per)
-        volume_per_list.append(volume_per)
-        open_per_list.append(open_per)
-        close_per_list.append(close_per)
-        high_per_list.append(high_per)
-        low_per_list.append(low_per)
-
-    count = len(date_time_per_list)
-
-    if (top_days_count_to_interpolate > count):
-        top_days_count_to_interpolate = count
-
-    interp_volume_per, interp_open_per, interp_close_per, interp_high_per, interp_low_per = interpolation.interpolate_prices_and_volume_by_distance(volume_per_list, open_per_list, close_per_list, high_per_list, low_per_list, dist, top_days_count_to_interpolate)
-
-    predicted_prices = prediction.project_percentage_change(last_price, interp_close_per)
-
-    return (predicted_prices, date_time_per_list[0], start[:top_days_count_to_interpolate], dist[:top_days_count_to_interpolate])
-
 
 
 start_date = datetime(2018, 1, 24, 9, 30)
@@ -70,7 +18,7 @@ symbol = "WEED.TO"
 #symbol = "MEDFF"
 #symbol = "AMZN"
 
-predicted_prices, times, closest_date_times, distances = get_linear_interpolation_prediction(symbol, start_date, end_date, days_count, interp_count)
+predicted_prices, times, closest_date_times, distances = prediction.get_linear_interpolation_prediction(symbol, start_date, end_date, days_count, interp_count)
 
 print predicted_prices
 print times
--- pyfin/utils/prediction.py
+++ pyfin/utils/prediction.py
@@ -1,6 +1,7 @@
 from stats import absolute, percentage
 from utils import time_op, sort_op
-
+from read_write import read
+from utils import interpolation
 
 
 def get_closest_distances_time_to_predict_and_distances(symbol, start_date, end_date, days_count):
@@ -40,3 +41,54 @@ def project_percentage_change(num, percentage_list):
         proj.append(b)
 
     return proj
+
+
+
+def get_linear_interpolation_prediction(symbol, from_date_time, to_date_time, days_count_to_analyse, top_days_count_to_interpolate):
+    
+    is_data_available, date_time_curr, volume_curr , opn_curr, close_curr, high_curr, low_curr = read.get_intraday_data(symbol, from_date_time, to_date_time, "1m")
+
+    if not (is_data_available):
+        return None
+
+    count_curr = len(close_curr)
+    last_price = close_curr[count_curr - 1]
+
+    start, end, dist = get_closest_distances_time_to_predict_and_distances(symbol, from_date_time, to_date_time, days_count_to_analyse)
+
+    count = len(start)
+
+    date_time_per_list = []
+    volume_per_list = []
+    open_per_list = []
+    close_per_list = []
+    high_per_list = []
+    low_per_list = []
+
+    for i in range (count):
+        is_data_available, date_time, volume , opn, close, high, low = read.get_intraday_data(symbol, start[i], end[i], "1m")
+        
+        if not (is_data_available):
+            continue
+
+        date_time_per, volume_per, open_per, close_per, high_per, low_per = percentage.get_percentage_change_in_intraday_prices(date_time, volume , opn, close, high, low)
+
+        date_time_per_list.append(date_time_per)
+        volume_per_list.append(volume_per)
+        open_per_list.append(open_per)
+        close_per_list.append(close_per)
+        high_per_list.append(high_per)
+        low_per_list.append(low_per)
+
+    count = len(date_time_per_list)
+
+    if (top_days_count_to_interpolate > count):
+        top_days_count_to_interpolate = count
+
+    interp_volume_per, interp_open_per, interp_close_per, interp_high_per, interp_low_per = interpolation.interpolate_prices_and_volume_by_distance(volume_per_list, open_per_list, close_per_list, high_per_list, low_per_list, dist, top_days_count_to_interpolate)
+
+    predicted_prices = project_percentage_change(last_price, interp_close_per)
+
+    return (predicted_prices, date_time_per_list[0], start[:top_days_count_to_interpolate], dist[:top_days_count_to_interpolate])
+
+
