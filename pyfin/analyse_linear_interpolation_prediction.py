281e21d8fb8d4ecc35ae72b8d5654927e57da9eb
Ara Danielyan <arikdan@gmail.com>
2018-01-24 22:00:50 -0500

    Implemented batch analysis

--- pyfin/analyse_linear_interpolation_prediction.py
+++ pyfin/analyse_linear_interpolation_prediction.py
@@ -5,18 +5,17 @@ end_date = datetime(2018, 1, 24, 10, 30)
 
 DAYS_TO_ANALYSE = 30
 
-
-#symbol = "WEED.TO"
-#symbol = "EMHTF"
-#symbol = "PRMCF"
-#symbol = "ACBFF"
-#symbol = "MEDFF"
-#symbol = "AMZN"
-symbol = "NVDA"
-
 ROOT_DIR = "results/analysis"
 
-prediction.analyse_linear_interpolation_prediction_performance(symbol, start_date, end_date, DAYS_TO_ANALYSE, ROOT_DIR)
+names = ["tickers/cannabis.txt", "tickers/cannot_be_positive.txt", "tickers/cannot_be_negative.txt", "tickers/battery.txt"]
+
+
+for name in names:
+    with open(name) as f:
+        tickers = f.read().splitlines()
+        count = len(tickers)
+        for i in range (count):
+            prediction.analyse_linear_interpolation_prediction_performance(tickers[i], start_date, end_date, DAYS_TO_ANALYSE, ROOT_DIR)
 
 
 
