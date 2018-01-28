1afbe7de9233e83e32b2a5d6bdf0e7beed5ffccc
Ara Danielyan <arikdan@gmail.com>
2018-01-28 13:13:16 -0500

    Added vizualisation of prediction

---
+++ pyfin/viz_prediction_from_closest_day.py
@@ -0,0 +1,44 @@
+from datetime import datetime, timedelta
+from utils import prediction
+import matplotlib.pyplot as plt
+from read_write import read
+
+
+start_date = datetime(2018, 1, 24, 9, 30)
+end_date = datetime(2018, 1, 24, 10, 30)
+
+days_count = 15
+
+interp_count = 3
+
+
+#symbol = "WEED.TO"
+#symbol = "EMHTF"
+#symbol = "PRMCF"
+#symbol = "ACBFF"
+#symbol = "MEDFF"
+symbol = "AMZN"
+
+real_start_date = end_date + timedelta(minutes = 1)
+real_end_date = end_date.replace(hour=15, minute=59)
+
+is_data_available, date_time_real, volume_real, opn_real, close_real, high_real, low_real = read.get_intraday_data(symbol, real_start_date, real_end_date, "1m")
+
+if not (is_data_available):
+    print ("No ground truth data available")
+    exit(0)
+
+
+predicted_prices, times, closest_date_times, distances = prediction.get_linear_interpolation_prediction(symbol, start_date, end_date, days_count, interp_count)
+
+print closest_date_times
+print distances
+
+plt.plot(times, predicted_prices)
+plt.plot(times, close_real, 'r')
+
+plt.gcf().autofmt_xdate()
+
+plt.show()
+
+
