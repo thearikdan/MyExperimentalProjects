201504f1e753c4d9c207cde83c3ced7d4670f254
IchibanKanobee <arikdan@gmail.com>
2018-05-09 18:16:25 -0400

    Added conveting from mesh to point cloud

---
+++ MeshToPointCloud/step1.py
@@ -0,0 +1,20 @@
+import sys
+sys.path.append("..")
+
+from utils import file_op
+from os.path import join, splitext
+import os
+
+SOURCE_DIR = '/raid/data/tinkercad/sample2/processed'
+DEST_DIR = '/raid/data/tinkercad/sample2/point_clouds/step_1'
+
+file_op.ensure_dir_exists(DEST_DIR)
+files = file_op.get_only_files(SOURCE_DIR)
+
+for f in files:
+    name, ext = splitext(f)
+    in_file = join(SOURCE_DIR, f)
+    out_file = join(DEST_DIR, name + ".xyz")
+    cmd = "meshlabserver -i " + in_file + " -o " + out_file + " -m vn"
+    os.system(cmd)
+
---
+++ utils/file_op.py
@@ -0,0 +1,35 @@
+from os import listdir
+from os.path import isfile, join, isdir
+import os
+import shutil
+
+
+def recreate_new_dir(directory):
+    if os.path.exists(directory):
+        shutil.rmtree(directory)
+    os.makedirs(directory)
+
+
+def ensure_dir_exists(directory):
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+
+def if_file_exists(filename):
+    if isfile(filename):
+        return true
+    else:
+        return false
+
+
+def get_only_files(directory): 
+    onlyfiles = [f for f in listdir(directory) if isfile(join(directory, f))]
+    return onlyfiles
+
+
+def get_only_dirs(directory): 
+    onlydirs = [f for f in listdir(directory) if isdir(join(directory, f))]
+    return onlydirs
+
+
+
