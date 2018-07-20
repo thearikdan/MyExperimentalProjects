c97d9b812b272fa37de5028a1239f2efc31f8d15
daniela <ara.danielyan@autodesk.com>
2018-07-20 16:38:13 -0400

    Added postgresql script

--- MeshToPointCloud/numpy_from_stl.py
+++ MeshToPointCloud/numpy_from_stl.py
@@ -2,11 +2,12 @@ import sys
 sys.path.append("..")
 
 from utils import file_op
+#import file_op
 from os.path import join, splitext
 import os
 from plyfile import (PlyData, PlyElement, make2d, PlyParseError, PlyProperty)
 import numpy as np
-from datetime import datetime
+#from datetime import datetime
 
 
 SOURCE_DIR = '/raid/data/chain_conversion_test'
@@ -17,35 +18,35 @@ POINT_COUNT_STR = str(POINT_COUNT)
 
 
 def stl_2_obj(f):
-    start = datetime.now()
+#    start = datetime.now()
     name, ext = splitext(f)
     in_file = join(SOURCE_DIR, f)
     out_file = join(DEST_DIR, name + ".obj")
     print ("Converting file " + f)
     cmd = "meshlabserver -i " + in_file + " -o " + out_file
     os.system(cmd)
-    end = datetime.now()
-    elapsed = end - start
-    print "STL to OBJ " + str(elapsed.microseconds / 1000) + " milliseconds"
+#    end = datetime.now()
+#    elapsed = end - start
+#    print "STL to OBJ " + str(elapsed.microseconds / 1000) + " milliseconds"
     return out_file
 
 
 def obj_2_pcd(f):
-    start = datetime.now()
+#    start = datetime.now()
     name, ext = splitext(f)
     in_file = join(SOURCE_DIR, f)
     out_file = join(DEST_DIR, name + ".pcd")
     print ("Converting file " + f)
     cmd = "pcl_mesh_sampling " + in_file + " " + out_file + " -n_samples " + POINT_COUNT_STR + " -no_vis_result"
     os.system(cmd)
-    end = datetime.now()
-    elapsed = end - start
-    print "\nOBJ to PCD " + str(elapsed.microseconds / 1000) + " milliseconds"
+#    end = datetime.now()
+#    elapsed = end - start
+#    print "\nOBJ to PCD " + str(elapsed.microseconds / 1000) + " milliseconds"
     return out_file
 
 
 def pcd_2_ply(f):
-    start = datetime.now()
+#    start = datetime.now()
     name, ext = splitext(f)
     in_file = join(SOURCE_DIR, f)
     out_file = join(DEST_DIR, name + ".ply")
@@ -53,9 +54,9 @@ def pcd_2_ply(f):
     cmd = "pcl_pcd2ply -format 0 " + in_file + " " + out_file #ascii
     #    cmd = "pcl_pcd2ply -format 1 " + in_file + " " + out_file #binary
     os.system(cmd)
-    end = datetime.now()
-    elapsed = end - start
-    print "\nPCD to PLY " + str(elapsed.microseconds / 1000) + " milliseconds"
+#    end = datetime.now()
+#    elapsed = end - start
+#    print "\nPCD to PLY " + str(elapsed.microseconds / 1000) + " milliseconds"
     return out_file
 
 
@@ -95,19 +96,23 @@ def stl_2_ply(f):
     return ply_file
 
 
-start = datetime.now()
-f = "/raid/data/chain_conversion_test/02tP152sBDk.stl"
-name, ext = splitext(f)
-ply_file = stl_2_ply(f)
+def main(f):
+#    print f
+#    start = datetime.now()
+    name, ext = splitext(f)
+    ply_file = stl_2_ply(f)
 
-num = ply_2_numpy(ply_file)
-data = pc_normalize(num)
+    num = ply_2_numpy(ply_file)
+    data = pc_normalize(num)
 
 
-out_file = join(DEST_DIR, name + ".txt")
-np.savetxt(out_file, data, fmt="%.5f")
-end = datetime.now()
-elapsed = end - start
+    out_file = join(DEST_DIR, name + ".txt")
+    np.savetxt(out_file, data, fmt="%.5f")
+#    end = datetime.now()
+#    elapsed = end - start
 
-print "Total elapsed time " + str(elapsed.microseconds / 1000) + " milliseconds"
+#    print "Total elapsed time " + str(elapsed.microseconds / 1000) + " milliseconds"
 
+
+if __name__ == "__main__":
+    main(sys.argv[1:][0])
---
+++ postgresql/populate_assets_table.py
@@ -0,0 +1,36 @@
+import sys
+sys.path.append("../")
+
+from utils import file_op
+import os
+import psycopg2
+
+DATA_DIR = "/raid/data/tinkercad/signatures/pointnet-autoencoder"
+
+files = file_op.get_only_files(DATA_DIR)
+count = len(files)
+
+for i in range (count):
+    files[i] = os.path.join(DATA_DIR, files[i])
+
+conn = psycopg2.connect("host=localhost dbname=tinkercad user=postgres password=tinkercad")
+cur = conn.cursor()
+
+
+files = file_op.get_only_files(DATA_DIR)
+count = len(files)
+
+for i in range (count):
+    name, _ = os.path.splitext(files[i])
+    files[i] = os.path.join(DATA_DIR, files[i])
+    cur.execute("INSERT INTO assets VALUES (%s, %s, %s, %s)", (10, 'hello@dataquest.io', 'Some Name', '123 Fake St.'))
+    print name
+
+#conn.commit()
+
+
+
+#all = cur.fetchall()
+
+#print all
+
