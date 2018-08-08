3270729a317a7632d32d42cf4ef195ab7e74220c
daniela <ara.danielyan@autodesk.com>
2018-08-08 17:37:54 -0400

    Added getting fundamental data

---
+++ pyfin/tests/get_fundamental_data.py
@@ -0,0 +1,14 @@
+#from https://stackoverflow.com/questions/40139537/scrape-yahoo-finance-financial-ratios
+#pip install requests
+import requests
+
+params = {"formatted": "true",
+        "crumb": "AKV/cl0TOgz", # works without so not sure of significance
+        "lang": "en-US",
+        "region": "US",
+        "modules": "defaultKeyStatistics,financialData,calendarEvents",
+        "corsDomain": "finance.yahoo.com"}
+
+r = requests.get("https://query1.finance.yahoo.com/v10/finance/quoteSummary/GSB", params=params)
+data = r.json()[u'quoteSummary']["result"][0]
+print data
