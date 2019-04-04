00daa9f1b42b1d6ee2fd8838f9ca85e1fb45603b
Ara Danielyan <arikdan@gmail.com>
2019-04-03 22:35:47 -0400

    Modified reduce_max

--- TensorFlow/reduce_max.py
+++ TensorFlow/reduce_max.py
@@ -1,6 +1,6 @@
 import tensorflow as tf
 
-
+'''
 init = tf.constant([[[1, 2, 3], [4, 5, 6], [7, 8, 9]], [[10, 11, 12], [13, 14, 15], [16, 17, 18]]])
 shape = tf.shape(init)
 red0 = tf.reduce_max(init, axis=0)
@@ -28,6 +28,23 @@ print ("---------------\n")
 print(sess.run(red2))
 print (sess.run(shape2))
 print ("---------------\n")
+'''
+
+init = tf.constant([[1, 5, 3], [4, 2, 6]])
+shape = tf.shape(init)
+red0 = tf.reduce_max(init, axis=0)
+shape0 = tf.shape(red0)
+
+sess = tf.Session()
+
+print (sess.run(init))
+print (sess.run(shape))
+print ("---------------\n")
+
+print(sess.run(red0))
+print (sess.run(shape0))
+print ("---------------\n")
+
 
 '''
 output
