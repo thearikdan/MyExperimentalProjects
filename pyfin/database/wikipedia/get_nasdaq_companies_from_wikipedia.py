ace6bba52e5453a6bfe197756adba13045376f53
Ara Danielyan <arikdan@gmail.com>
2020-03-02 22:06:38 -0500

    Added infobox data

--- pyfin/database/wikipedia/get_nasdaq_companies_from_wikipedia.py
+++ pyfin/database/wikipedia/get_nasdaq_companies_from_wikipedia.py
@@ -1,6 +1,6 @@
 import urllib.request as urllib2
 from bs4 import BeautifulSoup
-
+import json
 
 
 WIKI_URL_ROOT = "https://en.wikipedia.org"
@@ -38,9 +38,37 @@ def get_url_from_li(wiki_url_root, li):
         return ""
 
 
+
+def get_company_data_from_wiki_page(url):
+    #https: // stackoverflow.com / questions / 54120864 / scraping - wikipedia - infobox - when - table - cells - are - in -mixed - formats
+    info = {}
+    try:
+        page = urllib2.urlopen(url)
+        soup = BeautifulSoup(page,'html.parser')
+        tbl = soup.find("table", {"class": "infobox vcard"})
+        list_of_table_rows = tbl.findAll('tr')
+        for tr in list_of_table_rows:
+            th = tr.find("th")
+            td = tr.find("td")
+            if th is not None:
+                innerText = ''
+                for elem in td.recursiveChildGenerator():
+                    if isinstance(elem, str):
+                        innerText += elem.strip()
+                    elif elem.name == 'br':
+                        innerText += '\n'
+                info[th.text] = innerText
+        print(json.dumps(info, indent=1))
+        return info
+    except:
+        return info
+
+
+
 def get_companies_data(s, wiki_url_root, wiki_nasdaq_url):
     companies = []
     company_wiki_urls = []
+    companies_data = []
     page = urllib2.urlopen(wiki_nasdaq_url)
     soup = BeautifulSoup(page,'html.parser')
     lis = soup.find_all("li")
@@ -50,17 +78,22 @@ def get_companies_data(s, wiki_url_root, wiki_nasdaq_url):
             if len(url) > 0:
                 companies.append(li.get_text())
                 company_wiki_urls.append(url)
-    return companies, company_wiki_urls
+                data = get_company_data_from_wiki_page(url)
+                companies_data.append(data)
+    return companies, company_wiki_urls, companies_data
 
 
 companies = []
 wiki_company_uls = []
+companies_data = []
 
 for s in suffix_list:
     wiki_nasdaq_url = WIKI_NASDAQ_ROOT + s
-    companies_s, url_s  = get_companies_data(s, WIKI_URL_ROOT, wiki_nasdaq_url)
+    companies_s, url_s, data_s  = get_companies_data(s, WIKI_URL_ROOT, wiki_nasdaq_url)
     companies.extend(companies_s)
     wiki_company_uls.extend(url_s)
+    companies_data.extend(data_s)
 print(companies)
 print(wiki_company_uls)
+print(companies_data)
 print (len(companies))
