63a58e8ead64245c4e5cef006e8b276ff1239099
IchibanKanobee <arikdan@gmail.com>
2020-07-23 21:36:18 -0400

    Finished monitoring the stop and limit price

--- Python/timer_sms.py
+++ Python/timer_sms.py
@@ -13,22 +13,22 @@ import smtplib
 stop_event = threading.Event()
 limit_event = threading.Event()
 
-def start_monitor(server, stop_price, limit_price, email, password):
-    timer = threading.Timer(10, start_monitor, [server, stop_price, limit_price, email, password])
+def start_monitor(server, symbol, stop_price, limit_price, email, password):
+    timer = threading.Timer(10, start_monitor, [server, symbol, stop_price, limit_price, email, password])
     timer.start()
     
     price = random.uniform(0, 9)
-    message = "Reached price " + str(price)
+    message = "Symbol " + symbol + " reached price " + str(price)
     
     print (message)
     if not stop_event.is_set():
         if (price <= stop_price):
-            message = "Reached stop price " + str(price)
+            message = "Symbol " + symbol + " reached stop price " + str(price)
             server.sendmail('Markets Data Intelligence', '4168733699@msg.telus.com', message)
             stop_event.set()
     elif stop_event.is_set() and not limit_event.is_set():
         if (price <= limit_price):
-            message = "Reached limit price " + str(price)
+            message = "Symbol " + symbol + " reached limit price " + str(price)
             server.sendmail('Markets Data Intelligence', '4168733699@msg.telus.com', message)
             limit_event.set()
     elif stop_event.is_set() and limit_event.is_set():
@@ -45,9 +45,10 @@ with open(settings_file_name) as json_file:
 
 stop_price = 4
 limit_price = 3
+symbol = "TQQQ"
 print ("Started monitoring for stop price " + str(stop_price))
 server = smtplib.SMTP( "smtp.gmail.com", 587 )
 server.starttls()
 server.login( settings["email"], settings["email_password"])
 
-start_monitor(server, stop_price, limit_price, settings["email"], settings["email_password"])
+start_monitor(server, symbol, stop_price, limit_price, settings["email"], settings["email_password"])
--- pyfin/apps/general/monitor_price_down.py
+++ pyfin/apps/general/monitor_price_down.py
@@ -11,9 +11,12 @@ from datetime import datetime, timedelta
 
 import smtplib
 
+stop_event = threading.Event()
+limit_event = threading.Event()
 
-def start_monitor(server, symbol, stop_price, email, password):
-    threading.Timer(10, start_monitor, [server, symbol, stop_price, email, password]).start()
+
+def start_monitor(server, symbol, stop_price, limit_price, email, password):
+    threading.Timer(10, start_monitor, [server, symbol, stop_price, limit_price, email, password]).start()
     start_date = datetime.now() - timedelta(minutes=5)
     end_date = datetime.now()
 
@@ -33,8 +36,26 @@ def start_monitor(server, symbol, stop_price, email, password):
 
     message = "Symbol " + symbol + " reached price " + str(price) + " at " + last_time
     print (message)
-    if (price <= stop_price):
+    if not stop_event.is_set():
+        if (price <= stop_price):
+            message = "Symbol " + symbol + " reached stop price " + str(price) + " at " + last_time
+            server.sendmail('Markets Data Intelligence', '4168733699@msg.telus.com', message)
+            
+            stop_event.set()
+    
+    elif stop_event.is_set() and not limit_event.is_set():
+        if (price <= limit_price):
+            message = "Symbol " + symbol + " reached limit price " + str(price) + " at " + last_time
+            server.sendmail('Markets Data Intelligence', '4168733699@msg.telus.com', message)
+            
+            limit_event.set()
+    
+    elif stop_event.is_set() and limit_event.is_set():
+        message = "Stopped monitoring the price because limit was reached."
+        print (message)
         server.sendmail('Markets Data Intelligence', '4168733699@msg.telus.com', message)
+        timer.cancel()
+        timer.join()
 
 
 my_path = os.path.abspath(os.path.dirname(__file__))
@@ -44,9 +65,11 @@ with open(settings_file_name) as json_file:
 
 
 stop_price = 109.74
+limit_price = 107.02
+
 print ("Started monitoring for stop price " + str(stop_price))
 server = smtplib.SMTP( "smtp.gmail.com", 587 )
 server.starttls()
 server.login( settings["email"], settings["email_password"])
 
-start_monitor(server, "TQQQ", stop_price, settings["email"], settings["email_password"])
+start_monitor(server, "TQQQ", stop_price, limit_price, settings["email"], settings["email_password"])
