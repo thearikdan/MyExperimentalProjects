7dc2078c5ffb2718c3be2b0b4ab4c750fb785a71
IchibanKanobee <aikdan@gmail.com>
2020-08-03 22:49:20 -0400

    Continuing django users

--- webbasics-django/webbasics/webbasics/settings.py
+++ webbasics-django/webbasics/webbasics/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'marketsdataintelligence.apps.MarketsdataintelligenceConfig',
+    'users.apps.UsersConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
