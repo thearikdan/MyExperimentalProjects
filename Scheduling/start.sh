ebd6347b5d8f81de48552efc6fc27793279b04aa
IchibanKanobee <aikdan@gmail.com>
2020-10-09 14:23:51 -0400

    Removed scheduling

--- Scheduling/start.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-#echo "1"
-#/media/ara/HDD/MyProjects/Scheduling/test/start_test.sh
-#echo "2"
-python /media/ara/HDD/MyProjects/Scheduling/prn.py
-
--- Scheduling/test/start_test.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-cd /media/ara/HDD/MyProjects/Scheduling/test && gnome-terminal -e 'bash -ic "python test.py; exec bash"'
-
