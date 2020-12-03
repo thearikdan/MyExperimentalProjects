ff01bda99bdfc0750cfc75cc98e050f9b87d56d1
IchibanKanobee <arikdan@gmail.com>
2020-12-03 13:54:57 -0500

    Added scripts

---
+++ Scripts/path_variable.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+ROOT_DIR=media/ara/HDD
+echo $ROOT_DIR/virtualenvs/pyfin3/bin/activate
--- Scripts/rename.py
+++
@@ -1,8 +0,0 @@
-from os import listdir, rename
-from os.path import isfile, join
-mypath = "dog"
-
-for i, filename in enumerate(listdir(mypath)):
-    rename(mypath + "/" + filename, mypath + "/" + mypath + "_" + str(i) + ".jpg")
-
-
